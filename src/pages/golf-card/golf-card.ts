d5b655df3a22e64f12a32ec7c1a7a5834503b4f9
Cody Gates <cody.gates.89@gmail.com>
2018-07-25 23:09:17 -0600

    golf card looking good

--- src/pages/golf-card/golf-card.ts
+++ src/pages/golf-card/golf-card.ts
@@ -1,6 +1,6 @@
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Component} from '@angular/core';
-import {CoursesApiService} from "../../services/courses-api.service";
+import { Component } from '@angular/core';
+import { CoursesApiService } from "../../services/courses-api.service";
 
 @IonicPage()
 @Component({
@@ -13,10 +13,8 @@ export class GolfCardPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public coursesAPI: CoursesApiService){}
 
-  courseDetails:any;
-  pArray: Array<any> = [];
-  players;
-  holes;
+  playersArray: Array<any> = [];
+  courseDeets: any;
   holesIn: Array<any> = [];
   holesOut: Array<any> = [];
   tee: string;
@@ -29,29 +27,24 @@ export class GolfCardPage {
   totalHCPOut: number = 0;
   totalPIn : number = 0;
   totalPOut : number = 0;
+  golfPlayers;
+  holes;
 
   ionViewDidLoad() {
 
     console.log('ionViewDidLoad ScoreCardPage');
     this.params = this.navParams.data;
-    // console.log('the params ');
-    // console.log(  this.params );
-
     this.coursesAPI.returnCourseDetails(this.navParams.data.courseInfo.courseId.id).subscribe(data => {
-
-      this.courseDetails = data.data;
-      this.holes = this.courseDetails.holes;
+      this.courseDeets = data.data;
+      this.holes = this.courseDeets.holes;
       this.tee = this.navParams.data.courseInfo.tee.tee;
       this.teeIndex = this.navParams.data.courseInfo.tee.indexNum;
 
-      // console.log('tee index')
-      // console.log(this.teeIndex)
-
       for (let i = 0; i < 9; i++){
-        this.holesIn.push(this.courseDetails.holes[i]);
+        this.holesIn.push(this.courseDeets.holes[i]);
       }
       for (let i = 9; i < 18; i++){
-        this.holesOut.push(this.courseDetails.holes[i])
+        this.holesOut.push(this.courseDeets.holes[i])
       }
       for (let i = 0; i < this.holesIn.length; i++){
         this.totalYardsIn += Number(this.holesIn[i].teeBoxes[this.teeIndex].yards);
@@ -63,45 +56,29 @@ export class GolfCardPage {
         this.totalParOut += Number(this.holesOut[i].teeBoxes[this.teeIndex].par);
         this.totalHCPOut += Number(this.holesOut[i].teeBoxes[this.teeIndex].hcp);
       }
-      // console.log('holes In');
-      // console.log(this.holesIn);
-      // console.log('holes Out');
-      // console.log(this.holesOut);
-
-
-      // console.log('course details ');
-      // console.log( this.courseDetails );
-      this.players = this.params.numberOfPlayers;
-      // console.log(this.params.numberOfPlayers);
-      for(let i = 0; i < this.players; i++){
-        this.pArray.push('P ' + (i+1));
+      this.golfPlayers = this.params.numberOfPlayers;
+      for(let i = 0; i < this.golfPlayers; i++){
+        this.playersArray.push('P ' + (i+1));
       }
-      // console.log('this.pArray');
-      // console.log( this.pArray )
     });
   }
 
-  totalHoles() {
+  totalScoreOfHoles() {
 
     let courseLen = this.holes.length;
-    for (let p = 0; p < this.pArray.length; p++) {
+    for (let p = 0; p < this.playersArray.length; p++) {
       this.totalPIn = 0;
       this.totalPOut = 0;
-      if (p < this.pArray.length) {
+      if (p < this.playersArray.length) {
         for (let h = 0; h < courseLen; h++) {
           if (h <= 8) {
             let selectedInput = document.getElementById(`p${p}h${h}`);
-            // console.log(selectedInput);
             this.totalPIn += Number(selectedInput);
-
-            // console.log("total in "+this.totalPIn);
             document.getElementById('Player' + p + 'scoreIn').innerText = String(this.totalPIn);
-
           }
           if (h < courseLen && h >= 10) {
             let selectedInput = document.getElementById(`p${p}h${h}`);
             console.log(selectedInput);
-            // this.totalPOut += Number(selectedInput.value);
             console.log("total out "+this.totalPOut);
             document.getElementById('Player' + p + 'scoreOut').innerText = String(this.totalPOut);
           }
